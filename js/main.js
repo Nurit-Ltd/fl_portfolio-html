c66c856bc39e569ef561f211662234c6bc17c13b
thesumonart <thesumonart.bd@gmail.com>
2025-02-11 22:03:54 +0600

    Trade, Share Modal completed

--- js/main.js
+++ js/main.js
@@ -20,10 +20,10 @@ document.addEventListener("DOMContentLoaded", function () {
   actionMenu.style.display = "none";
 
   actionMenu.innerHTML = `
-                  <a href="#">Tx History</a>
-                  <a href="#">Share</a>
-                  <a href="#">Hide</a>
-                  <a href="#">Burn</a>`;
+                  <a class="tx-history-button" href="#">Tx History</a>
+                  <a class="share-button" href="#">Share</a>
+                  <a class="hide-button" href="#">Hide</a>
+                  <a class="burn-button" href="#">Burn</a>`;
 
   const actionMenuWrappers = document.querySelectorAll(".action-menu-wrapper");
   actionMenuWrappers.forEach((wrapper) => {
@@ -54,3 +54,91 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// Modals
+
+// Trade Modal
+
+let currentMode = "buy";
+const tradeModalOverlay = document.getElementById("tradeModalOverlay");
+const buyButton = document.getElementById("buyButton");
+const sellButton = document.getElementById("sellButton");
+const amountInput = document.getElementById("amountInput");
+const quickActionButton = document.getElementById("quickActionButton");
+
+function openModal() {
+  tradeModalOverlay.classList.add("active");
+}
+
+document.querySelectorAll(".trade-button").forEach((tradeButton) => {
+  tradeButton.addEventListener("click", openModal);
+});
+
+function closeModal() {
+  tradeModalOverlay.classList.remove("active");
+}
+
+function setMode(mode) {
+  currentMode = mode;
+  if (mode === "buy") {
+    buyButton.classList.remove("button-inactive");
+    buyButton.classList.add("buy-active");
+    sellButton.classList.remove("sell-active");
+    sellButton.classList.add("button-inactive");
+    quickActionButton.classList.remove("sell-active");
+    quickActionButton.classList.add("buy-active");
+    quickActionButton.textContent = "QUICK BUY";
+    amountInput.placeholder = "Amount to Buy (XRP)";
+  } else {
+    sellButton.classList.remove("button-inactive");
+    sellButton.classList.add("sell-active");
+    buyButton.classList.remove("buy-active");
+    buyButton.classList.add("button-inactive");
+    quickActionButton.classList.remove("buy-active");
+    quickActionButton.classList.add("sell-active");
+    quickActionButton.textContent = "QUICK SELL";
+    amountInput.placeholder = "Amount to Sell (XRP)";
+  }
+}
+
+function setAmount(xrp) {
+  amountInput.value = xrp;
+}
+
+// Close modal when clicking outside
+tradeModalOverlay.addEventListener("click", (e) => {
+  if (e.target === tradeModalOverlay) {
+    closeModal();
+  }
+});
+
+// Trade Modal
+
+// Share Modal
+
+const modal = document.getElementById("modal-share");
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".share-button").forEach((shareButton) => {
+    shareButton.addEventListener("click", () => {
+      modal.style.display = "flex";
+      document.body.style.overflow = "hidden";
+    });
+  });
+});
+
+const closeModalBtn = document.querySelector(".close-btn");
+
+closeModalBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+// Share Modal
+
+// Modals
